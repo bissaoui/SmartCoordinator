6c03bb7dbf2d618d21e122407eafe07f65907330
slimanisanaa <98154594+slimanisanaa@users.noreply.github.com>
2022-05-19 12:24:01 +0100

    Last Modification for UserController

---
+++ BackEnd/SmartBook/src/main/java/smart/book/cordinator/service/UserController.java
@@ -0,0 +1,63 @@
+package smart.book.cordinator.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import smart.book.cordinator.model.User;
+import smart.book.cordinator.repository.UserRepository;
+import smart.book.cordinator.service.UserServiceImpl;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/User")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAll(){
+        return userRepository.findAll();
+    }
+
+
+    @GetMapping("/{id}")
+    public User Get(@PathVariable("id") Long id){
+        return userRepository.findById(id).get();
+    }
+    @PostMapping
+    public User Get(@RequestParam("username") String username){
+        return userRepository.findUserByUsername(username);
+    }
+
+
+
+    @DeleteMapping("/{id}")
+    public void Delete(@PathVariable("id") Long id){
+         userRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public void Update(@PathVariable("id") Long id , @RequestBody User user){
+        User u = new User();
+        u=userRepository.findById(id).get();
+        if (user.getNom()!=null)
+            u.setNom(user.getNom());
+        if (user.getPrenom()!=null)
+         u.setPrenom(user.getPrenom());
+        if (user.getUsername()!=null)
+            u.setUsername(user.getUsername());
+        if (user.getDdn()!=null)
+            u.setDdn(user.getDdn());
+        userRepository.save(u);
+
+
+
+    }
+
+
+
+
+}
